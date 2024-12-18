d50412d062fba1fa1bbcb9695a711658a71dbf51
Romain ENEE <romaineneedev@gmail.COM>
2024-12-18 17:31:49 +0100

    finis

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "recipes/index"
   get "recipes/create"
   get "recipes/new"
+  get "products/loading"
   get "camera" => "products#camera"
   resources :products
   resources :user_recipes, only: [:index, :destroy, :create]
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get '/openai/create_recipe', to: 'openai#create_recipe'
 
   # Defines the root path route ("/")
   # root "posts#index"
