61a6763ab41a0dad3c3a85776fedab13a2de0d9d
Elizabeth Bonlarron <eli.bonlarron@gmail.com>
2024-12-19 11:34:03 +0100

    done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -14,7 +14,7 @@ class Recipe < ApplicationRecord
   def set_photos
     client = OpenAI::Client.new
     response = client.images.generate(parameters: {
-                                        prompt: "There is no people in the scene. Just a beautiful picture of a gourmet dish from above, of #{recipe[:name]} presented with elegance on a fine porcelain plate. The style is refined and minimalist, with a focus on vibrant warm colors and intricate details. The overall aesthetic is modern, gastronomic, and visually appetizing, as if featured in a high-end culinary magazine in a plate in the center of the picture. White backgroud.",
+                                        prompt: "There is no people in the scene. Just a beautiful picture of a gourmet dish from above, of #{self.name} presented with elegance on a fine porcelain plate. The style is refined and minimalist, with a focus on vibrant warm colors and intricate details. The overall aesthetic is modern, gastronomic, and visually appetizing, as if featured in a high-end culinary magazine in a plate in the center of the picture. White backgroud.",
                                         size: "256x256"
                                       })
 
